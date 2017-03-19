1fd845f9f9a27bc92f125f7ad748ae3748511d05
yupi <grafik@10g.pl>
2017-03-19 22:04:01 +0100

    poprawienie kodu

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
 public class MathQuiz {
-
     private Scanner scan = new Scanner(System.in);
 
     public boolean question1() {
@@ -12,7 +10,6 @@ public class MathQuiz {
         else
             return false;
     }
-
     public boolean question2() {
         System.out.println("jakie jest pole kwadratu o boku 12? ");
         int answer = getScan().nextInt();
@@ -21,7 +18,6 @@ public class MathQuiz {
         else
             return false;
     }
-
     public boolean question3() {
         System.out.println(" jaki jest pierwiastek kwadratowy ze 15129?");
         int answer = getScan().nextInt();
@@ -29,14 +25,11 @@ public class MathQuiz {
             return true;
         else
             return false;
-
-
     }
 
     public Scanner getScan() {
         return scan;
     }
-
     public void setScan(Scanner scan) {
         this.scan = scan;
     }
--- src/QuizTest.java
+++ src/QuizTest.java
@@ -1,37 +1,29 @@
 public class QuizTest {
     public static void main(String[] args) {
+        int sum = 0;
         MathQuiz mq = new MathQuiz();
         boolean q1 = mq.question1();
         boolean q2 = mq.question2();
         boolean q3 = mq.question3();
         if (q1) {
             System.out.println("odpowiedz 1 - dobrze");
+            sum++;
         } else {
             System.out.println("odpowiedz 1 - źle");
         }
         if (q2) {
             System.out.println("odpowiedz 2 - dobrze");
+            sum++;
         } else {
             System.out.println("odpowiedz 1 - źle");
         }
         if (q3) {
             System.out.println("odpowiedz 3 - dobrze");
+            sum++;
         } else {
             System.out.println("odpowiedz 1 - źle");
         }
-        int sum = 0;
-        if (q1) {
-            sum = (sum + 1);
-            sum++;
-        }
-        if (q2) {
-            sum = (sum + 1);
-            sum++;
-        }
-        if (q3) {
-            sum = (sum + 1);
-            sum++;
-        }
+
         System.out.println("odpowiedzi dobre " + sum + "/3");
     }
 }
