c3ef486638c9f93b83d5a16cda8a7bd6d0251722
yupi <grafik@10g.pl>
2017-03-19 21:55:57 +0100

    poprawienie kodu

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -5,8 +5,8 @@ public class MathQuiz {
     private Scanner scan = new Scanner(System.in);
 
     public boolean question1() {
-        System.out.print("ile jest 3*5 = ");
-        int answer = scan.nextInt();
+        System.out.println("ile jest 3*5 = ");
+        int answer = getScan().nextInt();
         if (answer == 15)
             return true;
         else
@@ -15,7 +15,7 @@ public class MathQuiz {
 
     public boolean question2() {
         System.out.println("jakie jest pole kwadratu o boku 12? ");
-        int answer = scan.nextInt();
+        int answer = getScan().nextInt();
         if (answer == 144)
             return true;
         else
@@ -24,7 +24,7 @@ public class MathQuiz {
 
     public boolean question3() {
         System.out.println(" jaki jest pierwiastek kwadratowy ze 15129?");
-        int answer = scan.nextInt();
+        int answer = getScan().nextInt();
         if (answer == 123)
             return true;
         else
@@ -33,4 +33,11 @@ public class MathQuiz {
 
     }
 
+    public Scanner getScan() {
+        return scan;
+    }
+
+    public void setScan(Scanner scan) {
+        this.scan = scan;
+    }
 }
--- src/QuizTest.java
+++ src/QuizTest.java
@@ -4,21 +4,34 @@ public class QuizTest {
         boolean q1 = mq.question1();
         boolean q2 = mq.question2();
         boolean q3 = mq.question3();
-
-        if (q1 == mq.question1()) {
+        if (q1) {
             System.out.println("odpowiedz 1 - dobrze");
         } else {
-            System.out.println("odpowiedz 1- źle");
+            System.out.println("odpowiedz 1 - źle");
         }
-        if (q2 == mq.question2()) {
+        if (q2) {
             System.out.println("odpowiedz 2 - dobrze");
         } else {
-            System.out.println("odpowiedz 1- źle");
+            System.out.println("odpowiedz 1 - źle");
         }
-        if (q3 == mq.question3()) {
+        if (q3) {
             System.out.println("odpowiedz 3 - dobrze");
         } else {
-            System.out.println("odpowiedz 1- źle");
+            System.out.println("odpowiedz 1 - źle");
         }
+        int sum = 0;
+        if (q1) {
+            sum = (sum + 1);
+            sum++;
+        }
+        if (q2) {
+            sum = (sum + 1);
+            sum++;
+        }
+        if (q3) {
+            sum = (sum + 1);
+            sum++;
+        }
+        System.out.println("odpowiedzi dobre " + sum + "/3");
     }
 }
